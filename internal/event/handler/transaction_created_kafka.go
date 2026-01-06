1f490b33e0507ad3d7d5ea71a6383e11719cf50c
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2026-01-06 10:58:04 -0300

    implementado TransactionHandler

---
+++ internal/event/handler/transaction_created_kafka.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/leomoritz/fullcycle-arquitetura-microsservicos/pkg/events"
+	"github.com/leomoritz/fullcycle-arquitetura-microsservicos/pkg/kafka"
+)
+
+type TransactionCreatedKafkaHandler struct {
+	Kafka *kafka.Producer
+}
+
+func NewTransactionCreatedKafkaHandler(kafkaProducer *kafka.Producer) *TransactionCreatedKafkaHandler {
+	return &TransactionCreatedKafkaHandler{
+		Kafka: kafkaProducer,
+	}
+}
+
+func (h *TransactionCreatedKafkaHandler) Handle(message events.EventInterface, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+	err := h.Kafka.Publish(message, nil, "transactions") // mensagem, chave e nome do tópico
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Transaction created event published to Kafka topic 'transactions' with data:", message)
+}
